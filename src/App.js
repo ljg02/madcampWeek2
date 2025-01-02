7c719ddde114eeaa405e38cb89776c5550d91b2a
ljg02 <gyu021211@gmail.com>
2025-01-02 19:33:10 +0900

    youtube UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FaSearch, FaBars, FaUserCircle, FaVideo, FaBell } from "react-icons/fa";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      {/* Header */}
+      <header className="header">
+        <div className="header__left">
+          <FaBars className="icon" />
+          <img
+            src="https://www.youtube.com/img/desktop/yt_1200.png"
+            alt="YouTube Logo"
+            className="logo"
+          />
+        </div>
+        <div className="header__center">
+          <input type="text" placeholder="Search" className="search-bar" />
+          <button className="search-button">
+            <FaSearch />
+          </button>
+        </div>
+        <div className="header__right">
+          <FaVideo className="icon" />
+          <FaBell className="icon" />
+          <FaUserCircle className="icon" />
+        </div>
       </header>
+
+      {/* Sidebar */}
+      <div className="sidebar">
+        <ul>
+          <li>Home</li>
+          <li>Trending</li>
+          <li>Subscriptions</li>
+          <li>Library</li>
+          <li>History</li>
+        </ul>
+      </div>
+
+      {/* Main Content */}
+      <div className="main-content">
+        <div className="video-card">
+          <img
+            src="https://via.placeholder.com/320x180"
+            alt="Thumbnail"
+            className="thumbnail"
+          />
+          <div className="video-info">
+            <img
+              src="https://via.placeholder.com/36"
+              alt="Channel Icon"
+              className="channel-icon"
+            />
+            <div className="video-text">
+              <h4>Video Title</h4>
+              <p>Channel Name</p>
+              <p>1M views • 1 day ago</p>
+            </div>
+          </div>
+        </div>
+        {/* Add more video cards here */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
